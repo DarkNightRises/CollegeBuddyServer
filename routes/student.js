c0bb17be6cb09f3e055c45c01a9707916f16b94f
KKS161994 <kartikeyshrivastava@gmail.com>
2016-12-22 15:23:50 +0530

    Created GCM id update api for student and teacher

--- routes/student.js
+++ routes/student.js
@@ -1,3 +1,10 @@
+/***
+Module for Student API
+***/
+
+
+
+//Requirements
 var express = require('express');
 var app = express();
 var fs = require("fs");
@@ -7,6 +14,7 @@ var router = express.Router();
 var Promise = require('promise')
 var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/collegebuddy'
 
+//For parsing post json data in API
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 module.exports = function(app){
@@ -14,71 +22,78 @@ module.exports = function(app){
 	app.get('/print', function(req, res){
 		res.end('Hello babay')
 	});
-	app.post('/api/loginStudent',function(req,res){
-		pg.connect(connectionString, function(err,client,done){	
-			if (err){
-				done();
-				console.log(err)
-				return res.status(500).json({success: false, data: 'Connection to database failed'});
-				
-			}
-			var email = req.body.email;
-			var password = req.body.password;
-			var queryString = 'Select * from Student where email=\''+email+'\' and password=\''+password+'\'';
-			var promise = executeQuery(queryString,client);
-			promise.then(function(value){
-				if(value.length>0){
-					return res.status(200).json({success:true,data: value});
-				}
-				else
-					return res.status(200).json({success:false, data: 'Invalid email id or password'});
-			});});
-	});
-	app.post('/api/signupStudent', function(req, res){
-		var results = [];
-		var data = {
-			name: req.body.name, 
-			branch: req.body.branch,
-			section: req.body.section,
-			year: req.body.year,
-			email: req.body.email, 
-			mobile_no: req.body.mobile_no,
-			password: req.body.password,
-			college_name: req.body.college_name
-		};
-		pg.connect(connectionString, function(err,client,done){
-			if(err){
-				done();
-				console.log(err)
-				return res.status(500).json({success: false, data: 'Connection to database failed'});
-			}
-			var queryString = ('SELECT * from Student where email = \''+data.email+'\'');
-			var promise=executeQuery(queryString,client);
-			console.log('Value is qs '+queryString);
 
 
-			promise.then(function(value){
-				console.log('Value is '+value);
-				results = value
-				if(value.length>0){
-					done();
-					return res.status(500).json({success: false, data:'Email already exists'}); 
-				}
+//API for Login Student
+app.post('/api/loginStudent',function(req,res){
+	pg.connect(connectionString, function(err,client,done){	
+		if (err){
+			done();
+			console.log(err)
+			return res.status(500).json({success: false, data: 'Connection to database failed'});
+
+		}
+		var email = req.body.email;
+		var password = req.body.password;
+		var queryString = 'Select * from Student where email=\''+email+'\' and password=\''+password+'\'';
+		var promise = executeQuery(queryString,client);
+		promise.then(function(value){
+			if(value.length>0){
+				return res.status(200).json({success:true,data: value});
+			}
+			else
+				return res.status(200).json({success:false, data: 'Invalid email id or password'});
+		});});
+});
+
+//API for student signup
+app.post('/api/signupStudent', function(req, res){
+	var results = [];
+	var data = {
+		name: req.body.name, 
+		branch: req.body.branch,
+		section: req.body.section,
+		year: req.body.year,
+		email: req.body.email, 
+		mobile_no: req.body.mobile_no,
+		password: req.body.password,
+		college_name: req.body.college_name
+	};
+	pg.connect(connectionString, function(err,client,done){
+		if(err){
+			done();
+			console.log(err)
+			return res.status(500).json({success: false, data: 'Connection to database failed'});
+		}
+		var queryString = ('SELECT * from Student where email = \''+data.email+'\'');
+		var promise=executeQuery(queryString,client);
+		console.log('Value is qs '+queryString);
+
+
+		promise.then(function(value){
+			console.log('Value is '+value);
+			results = value
+			if(value.length>0){
+				done();
+				return res.status(500).json({success: false, data:'Email already exists'}); 
+			}
 				//queryString = ('INSERT INTO Student(name, branch,section, college, year, email, mob_no, password) values($1, $2, $3, $4, $5, $6, $7, $8)',
 				//[data.name, data.branch, data.section, data.college_name, data.year, data.email, data.mobile_no, data.password]);
 
-			var insertPromise = executeSignUpQuery(data,client);
-			insertPromise.then(function(value)
-			{
-				done();
-				return res.status(200).json(data);
-			});
-
+		var insertPromise = executeSignUpQuery(data,client);
+		insertPromise.then(function(value)
+		{
+			done();
+			return res.status(200).json(data);
 		});
 
+	});
 
-		});
+
+	});
 });
+
+//Promises for Select Query
 function executeQuery(quer,client){
 	console.log('Inside ec'+quer)
 	return new Promise(function (resolve,reject){
@@ -93,6 +108,8 @@ function executeQuery(quer,client){
 		});
 	});
 }
+
+//Promises to check during sign up whether email already exists
 function executeEmailQuery(quer,client,data){
 	console.log('Inside ec')
 	return new Promise(function (resolve,reject){
@@ -107,11 +124,13 @@ function executeEmailQuery(quer,client,data){
 		});
 	});
 }
+
+//Promises to post data during signup
 function executeSignUpQuery(data,client){
 	console.log('Inside IC')
 	return new Promise(function (resolve,reject){
 		var results = [];
-		var newquery = client.query('INSERT INTO Student(name, branch,section, college, year, email, mob_no, password) values($1, $2, $3, $4, $5, $6, $7, $8)',
+		var newquery = client.query('INSERT INTO Student(name, branch,section, college, year, email, mob_no, password, gcm_id, device_id) values($1, $2, $3, $4, $5, $6, $7, $8, 0, 0)',
 			[data.name, data.branch, data.section, data.college_name, data.year, data.email, data.mobile_no, data.password]);
 		return resolve('done');
 	});
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-//  var stormpath = require('stormpath')
+/***
+Module for managing Teacher API
+***/
 var express = require('express');
 var app = express();
 var fs = require("fs");
@@ -11,113 +13,171 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.get('/listUsers', function (req, res) {
  fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
-   console.log( data );
-   res.end( data );
+	 console.log( data );
+	 res.end( data );
  });
 })
 
 app.post('/api/signupTeacher', function(req, res) {
-  var results = [];
-  // Grab data from http request
-  
-  var data = {
-    name: req.body.name, 
-    email: req.body.email, 
-    mobile_no: req.body.mobile_no,password: 
-    req.body.password,
-    college_name: req.body.college_name};
-    
-    console.log(data)
-  // Get a Postgres client from the connection pool
-  pg.connect(connectionString, function(err, client, done) {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    var query = client.query('SELECT * from Teacher where email= $1 ',[data.email]);
-    // Stream results back one row at a time
-    query.on('row', function(row) {
-      results.push(row);
-    });
-    query.on('end', function()  {
-      console.log(results)
-      
+	var results = [];
+	// Grab data from http request
+	
+	var data = {
+		name: req.body.name, 
+		email: req.body.email, 
+		mobile_no: req.body.mobile_no,password: 
+		req.body.password,
+		college_name: req.body.college_name};
+		
+		console.log(data)
+	// Get a Postgres client from the connection pool
+	pg.connect(connectionString, function(err, client, done) {
+		// Handle connection errors
+		if(err) {
+			done();
+			console.log(err);
+			return res.status(500).json({success: false, data: err});
+		}
+		var query = client.query('SELECT * from Teacher where email= $1 ',[data.email]);
+		// Stream results back one row at a time
+		query.on('row', function(row) {
+			results.push(row);
+		});
+		query.on('end', function()  {
+			console.log(results)
+			
 
-      if(results.length>0){
-        done();
-        
-        return res.status(200).json({success: false, data:'Email already exists'}); 
-      }
-      else{
-        results = [];
-    // SQL Query > Insert Data
-    client.query('INSERT INTO Teacher(name, email, mob_no, password, college) values($1, $2, $3, $4, $5)',
-      [data.name, data.email, data.mobile_no,data.password,data.college_name]);
-    // SQL Query > Select Data
-    query = client.query('SELECT * from Teacher where email= $1 AND password= $2',[data.email,data.password]);
-    // Stream results back one row at a time
-    query.on('row', function(row) {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', function()  {
-      done();
-      return res.json({sucess: true, data: results});
-    });
-  }
+			if(results.length>0){
+				done();
+				
+				return res.status(200).json({success: false, data:'Email already exists'}); 
+			}
+			else{
+				results = [];
+		// SQL Query > Insert Data
+		client.query('INSERT INTO Teacher(name, email, mob_no, password, college, gcm_id, device_id) values($1, $2, $3, $4, $5, 0, 0)',
+			[data.name, data.email, data.mobile_no,data.password,data.college_name]);
+		// SQL Query > Select Data
+		query = client.query('SELECT * from Teacher where email= $1 AND password= $2',[data.email,data.password]);
+		// Stream results back one row at a time
+		query.on('row', function(row) {
+			results.push(row);
+		});
+		// After all data is returned, close connection and return results
+		query.on('end', function()  {
+			done();
+			return res.json({sucess: true, data: results});
+		});
+	}
 });
-  });
+	});
 });
 app.post('/api/loginTeacher',function(req,res){
-  var results = [];
-  var email=req.body.email;
-  var password=req.body.password;
- // console.log("User name = "+user_name+", password is "+password);
- // res.end("yes");
+	var results = [];
+	var email=req.body.email;
+	var password=req.body.password;
  pg.connect(connectionString, function(err, client, done) {
-    // Handle connection errors
-    if(err) {
-      done();
-      console.log(err);
-      return res.status(500).json({success: false, data: err});
-    }
-    // SQL Query > Insert Data
-    // SQL Query > Select Data
-    const query = client.query('SELECT * from Teacher where email= $1 AND password= $2',[email,password]);
-    // Stream results back one row at a time
-    query.on('row', function(row) {
-      results.push(row);
-    });
-    // After all data is returned, close connection and return results
-    query.on('end', function()  {
-      done();
-      if(results>0){
-        return res.json(results);
-        
-      }
-      else{
-        return res.end("No such user exists");
-      }
-    });
-  });
+		// Handle connection errors
+		if(err) {
+			done();
+			console.log(err);
+			return res.status(500).json({success: false, data: err});
+		}
+		// SQL Query > Insert Data
+		// SQL Query > Select Data
+		const query = client.query('SELECT * from Teacher where email= $1 AND password= $2',[email,password]);
+		// Stream results back one row at a time
+		query.on('row', function(row) {
+			console.log("Teacher is "+row);
+			results.push(row);
+		});
+		// After all data is returned, close connection and return results
+		query.on('end', function()  {
+			done();
+			if(results.length>0){
+				return res.json(results);
+				
+			}
+			else{
+				return res.end("No such user exists");
+			}
+		});
+	});
 });
+
+//API for gcm id update
+app.post('/api/gcmidUpdate',function(req,res){
+	var data = [];
+	data.gcm_id= req.body.gcm_id;
+	data.device_id = req.body.device_id;
+	/***
+	Dataflow 0 for Teacher 
+	Dataflow 1 for Student
+	***/
+	data.dataflow = req.body.dataflow;					
+	data.email = req.body.email;
+	pg.connect(connectionString,function(err,client,done){
+	if(err){
+			done();
+			console.log(err);
+			return res.status(500).json({success: false, data: err});
+	}
+	var insertionPromise = insertinTable(data,client);
+	insertionPromise.then(function(value){
+		console.log('final value of promise is '+value);
+		if(value == 'done'){
+			done();
+			return res.status(200).json({success: true,data: 'Insert successfull'})
+		}
+		else{	
+			done();
+			return res.status(400).json({success: false,data: 'Insert Unsuccessfull'})
+			}
+		}).catch(function(e){
+			console.log('Got error '+e);
+		});
+	});
+});
+
+
+//Test API for hosting check
 app.get('/checkApi',function(req,res)
 {
-  console.log("Hosted on Heoku"); 
-  return res.end("Got result");
+	console.log("Hosted on Heoku"); 
+	return res.end("Got result");
 }
-
 );
 
+//Promise for insert query
+function insertinTable(data,client){
+	return new Promise(function	(resolve,reject)
+		{
+
+			var connectionquery ;
+			if(data.dataflow == 0){
+				connectionquery = client.query('Update Teacher SET gcm_id=$1,device_id=$2 where email=$3',[data.gcm_id,data.device_id,data.email]);
+			}
+			else if(data.dataflow == 1){
+				connectionquery = client.query('Update Student SET gcm_id=$1,device_id=$2 where email=$3',[data.gcm_id,data.device_id,data.email]);	
+			}
+			connectionquery.on('end',function(){
+			return resolve('done');	
+			}
+			);
+		}
+		);
+}
+
+//Route for student API
 require('./routes/student')(app);
+
+
 var server = app.listen(process.env.PORT || 8082, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+	var host = server.address().address
+	var port = server.address().port
 
-  console.log("Example app listening at http://%s:%s", host, port)
+	console.log("Example app listening at http://%s:%s", host, port)
 
 })
 
